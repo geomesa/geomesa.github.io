77e86af9ca30ca232ddc9cf05b3b010fd4396bdd
mrk239 <mkrofcheck@ccri.com>
2014-04-22 10:25:49 -0400

    Fixing header bar on smaller screens.

--- js/app.js
+++ js/app.js
@@ -13,4 +13,12 @@ $(function () {
         $navContent.toggleClass('collapsed');
     });
 
+    $('body').click(function (e) {
+        var $t = $(e.target),
+            isNotNav = $t.closest('.sidebar').length === 0;
+
+        if(isNotNav || $t.is('a')) {
+            $navContent.addClass('collapsed');
+        }
+    });
 });
