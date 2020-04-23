2c6845c7b6508abd9ca9b7730c3d0f096f584f24
Development Infrastructure <infrastructure@ccri.com>
2020-04-23 10:09:38 +0000

    Jenkins documentation update 2020-04-23 10:09

--- documentation/current/_static/documentation_options.js
+++ documentation/current/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.0-m0',
+    VERSION: '3.0.0-SNAPSHOT',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
